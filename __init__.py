abbb3950f7492505802ad52a772f7b320845cfee
oshtz <omershatzberg@gmail.com>
2025-04-09 12:05:21 +0300

    Remove ImageOverlayNode references from the initialization module

--- __init__.py
+++ __init__.py
@@ -7,7 +7,6 @@ else:
     from .nodes.lora_switcher_20 import LoRASwitcherNode20 as LoRASwitcherNode20
     from .nodes.lora_switcher_40 import LoRASwitcherNode40
     from .nodes.llm_aio import LLMAIONode
-    from .nodes.image_overlay import ImageOverlayNode
     from .nodes.string_splitter import StringSplitterNode
     from .nodes.aspect_ratio import EasyAspectRatioNode
 
@@ -16,7 +15,6 @@ else:
         "LoRASwitcherNode20": LoRASwitcherNode20,
         "LoRASwitcherNode40": LoRASwitcherNode40,
         "LLMAIONode": LLMAIONode,
-        "ImageOverlayNode": ImageOverlayNode,
         "StringSplitterNode": StringSplitterNode,
         "EasyAspectRatioNode": EasyAspectRatioNode
     }
@@ -26,7 +24,6 @@ else:
         "LoRASwitcherNode20": LoRASwitcherNode20.TITLE,
         "LoRASwitcherNode40": LoRASwitcherNode40.TITLE,
         "LLMAIONode": LLMAIONode.TITLE,
-        "ImageOverlayNode": ImageOverlayNode.TITLE,
         "StringSplitterNode": StringSplitterNode.TITLE,
         "EasyAspectRatioNode": EasyAspectRatioNode.TITLE
     }
